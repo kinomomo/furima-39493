bb9bea59d2cf1ec737c0cc4b0c859c792412d1d4
kinomomo <snowveil6@gmail.com>
2023-08-02 15:49:30 +0900

    :order削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :order, :token
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
