7434db2e996067b040aaf9ca8caab80b4dbab3c9
kinomomo <snowveil6@gmail.com>
2023-08-02 19:38:11 +0900

    電話番号のバリデーション記述変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. not Include hyphen(-)' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
     validates :token
   end
 
